b06741d0fd25f51ea3ad7eea16e80254460f1b16
rolypoly2020 <2667294317@qq.com>
2020-02-12 16:48:36 +0800

    代码格式优化

--- 打印正方形.c
+++ 打印正方形.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
-int main()
-{
-    int N;
-    char C;
-    scanf("%d %c", &N, &C);
-    if (N < 3 || N > 20)
-        printf("请输入3到20间的数");
-    else
-    {
-        for (int i = 1; i <= N / 2; i++)
-        {
-            if (i == 1 || i == N / 2)
-            {
-                for (int j = 1; j <= N; j++)
-                    printf("%c", C);
-            }
-            else
-            {
-                printf("\n%c", C);
-                for (int k = 1; k <= N - 2; k++)
-                    printf(" ");
-                printf("%c\n", C);
-            }
-        }
+int main() {
+  int N;
+  char C;
+  scanf("%d %c", &N, &C);
+  if (N < 3 || N > 20)
+    printf("请输入3到20间的数");
+  else {
+    for (int i = 1; i <= N / 2; i++) {
+      if (i == 1 || i == N / 2) {
+        for (int j = 1; j <= N; j++)
+          printf("%c", C);
+      } else {
+        printf("\n%c", C);
+        for (int k = 1; k <= N - 2; k++)
+          printf(" ");
+        printf("%c\n", C);
+      }
     }
-    return 0;
+  }
+  return 0;
 }
